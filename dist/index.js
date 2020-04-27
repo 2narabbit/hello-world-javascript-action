a05f8d0e0db55c8015455db93d800eedcfef16ad
ellie.rabbit <ellie.rabbit@daumkakao.com>
2020-04-27 13:33:21 +0900

    test

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {execSync} = require('child_process');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hello ${execSync(`cat ./LICENSE`).toString()}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
