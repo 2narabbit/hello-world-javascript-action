7c65f101d2f1fbf1d5c7bf1b07225425e76a3d69
ellie.rabbit <ellie.rabbit@daumkakao.com>
2020-04-27 13:47:53 +0900

    test

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,9 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   console.log(`dirname : ${console.log(__dirname)}`);
   console.log(`cwd : ${process.cwd()}`);
+  console.log(`ls : ${execSync(`ls al ./`).toString()}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
